0af660e1f344bf113f7049aa5646a13f84a9ba01
szbgreg <szbgreg@gmail.com>
2020-11-22 15:22:02 +0100

    Change mongoose edit command to findByIdAndUpdate

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.get('/campgrounds/:id/edit', async (req, res) => {
 
 app.put('/campgrounds/:id', async (req, res) => {
   const { id } = req.params;
-  const campground = await Campground.findOneAndUpdate(id, {
+  const campground = await Campground.findByIdAndUpdate(id, {
     ...req.body.campground,
   });
   res.redirect(`/campgrounds/${campground._id}`);
