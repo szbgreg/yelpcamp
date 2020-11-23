29196128fb60b971fcf7254fb8dd7f61490d1032
szbgreg <szbgreg@gmail.com>
2020-11-23 19:38:57 +0100

    Adding images

--- seeds/index.js
+++ seeds/index.js
@@ -21,9 +21,14 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: 'https://source.unsplash.com/collection/483251',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam molestias accusamus cumque ex esse quas similique, nesciunt exercitationem excepturi quo? Et aliquid earum aspernatur fuga saepe illo voluptas optio obcaecati.',
+      price,
     });
     await camp.save();
   }
