33efb4bec64883020f814dcef96a0f02f0009ea2
szbgreg <szbgreg@gmail.com>
2020-11-30 19:37:53 +0100

    Serving static assets

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
+  useFindAndModify: false,
 });
 
 const db = mongoose.connection;
@@ -28,6 +29,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static('public'));
 
 app.use('/campgrounds', campgrounds);
 app.use('/campgrounds/:id/reviews', reviews);
