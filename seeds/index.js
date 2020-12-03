88fd4e9f054bfb6866b8dc73bba8c417eab820a7
szbgreg <szbgreg@gmail.com>
2020-12-03 13:10:49 +0100

    Fixed images url in seeds file

--- seeds/index.js
+++ seeds/index.js
@@ -6,7 +6,7 @@ const Campground = require('../models/campground');
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useNewUrlParser: true,
   useCreateIndex: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 });
 
 const db = mongoose.connection;
@@ -23,13 +23,24 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
-      author: '5fc679a83067b12f60867930',
+      author: '5fc8d4c31240a332f0239b38',
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
-      image: 'https://source.unsplash.com/collection/483251',
+      images: [
+        {
+          url:
+            'https://res.cloudinary.com/dzm0huf8s/image/upload/v1606996837/YelpCamp/v32akhtzogmpdql7fi8s.jpg',
+          filename: 'YelpCamp/v32akhtzogmpdql7fi8s'
+        },
+        {
+          url:
+            'https://res.cloudinary.com/dzm0huf8s/image/upload/v1606996950/YelpCamp/uxfpzafdqb67lotdfujt.jpg',
+          filename: 'YelpCamp/uxfpzafdqb67lotdfujt'
+        }
+      ],
       description:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam molestias accusamus cumque ex esse quas similique, nesciunt exercitationem excepturi quo? Et aliquid earum aspernatur fuga saepe illo voluptas optio obcaecati.',
-      price,
+      price
     });
     await camp.save();
   }
