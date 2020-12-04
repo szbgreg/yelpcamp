1778ff0932781a414ad01d268af0f633667010d1
szbgreg <szbgreg@gmail.com>
2020-12-04 14:11:29 +0100

    Adding helmet & config ContentSecurityPolicy

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const session = require('express-session');
 const flash = require('connect-flash');
+const helmet = require('helmet');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const passport = require('passport');
@@ -22,7 +23,7 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
-  useFindAndModify: false,
+  useFindAndModify: false
 });
 
 const db = mongoose.connection;
@@ -48,12 +49,65 @@ const sessionConfig = {
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
+    /* secure: true, */
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-    maxAge: 1000 * 60 * 60 * 24 * 7,
-  },
+    maxAge: 1000 * 60 * 60 * 24 * 7
+  }
 };
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(helmet());
+
+const scriptSrcUrls = [
+  'https://stackpath.bootstrapcdn.com/',
+  'https://api.tiles.mapbox.com/',
+  'https://api.mapbox.com/',
+  'https://cdnjs.cloudflare.com/',
+  'https://cdn.jsdelivr.net/',
+  'https://kit.fontawesome.com/',
+  'https://code.jquery.com/'
+];
+
+const stylesSrcUrls = [
+  'https://stackpath.bootstrapcdn.com/',
+  'https://api.tiles.mapbox.com/',
+  'https://api.mapbox.com/',
+  'https://fonts.googleapis.com',
+  'https://kit-free.fontawesome.com/',
+  'https://use.fontawesome.com/',
+  'https://cdn.jsdelivr.net/'
+];
+
+const connectSrcUrls = [
+  'https://api.mapbox.com/',
+  'https://a.tiles.mapbox.com/',
+  'https://b.tiles.mapbox.com/',
+  'https://events.tiles.mapbox.com/',
+  'https://events.mapbox.com/'
+];
+
+const fontSrcUrls = [];
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: [],
+      connectSrc: ["'self'", ...connectSrcUrls],
+      scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+      styleSrc: ["'unsafe-inline'", "'self'", ...stylesSrcUrls],
+      workerSrc: ["'self'", 'blob:'],
+      objectSrc: [],
+      imgSrc: [
+        "'self'",
+        'blob:',
+        'data:',
+        `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/`,
+        'https://images.unsplash.com/'
+      ],
+      fontSrc: ["'self'", ...fontSrcUrls]
+    }
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
